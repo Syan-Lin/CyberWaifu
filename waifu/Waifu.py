6d9d28746da964676bb25730a8c1d09cd023e79c
L1Xu4n <106089276+L1Xu4n@users.noreply.github.com>
2023-06-07 00:44:26 +0800

    Update Waifu.py

--- waifu/Waifu.py
+++ waifu/Waifu.py
@@ -237,7 +237,7 @@ Conversation:
       The requirements:
        - Output ONLY in Chinese.
        - Describe what the whole conversation is doing with 10 Chinese keywords in the first line. JUST send the KEYWORDS DIRECTLY:
-       - What memories do you think should be important to remember from the conversation? Describe as much as possible in Chinese within 300 tokens, Each action should include the time.INCLUDEING TIME AND DATE. JUST send the SUMMARIZE DERECTLY:"""
+       - What memories do you think should be important to remember from the conversation? Describe as much as possible in Chinese within 300 tokens, Each action should include the time.INCLUDEING TIME AND DATE. JUST send the SUMMARIZE DIRECTLY:"""
         print('开始总结')
         summary = self.brain.think_nonstream([SystemMessage(content=prompt_template)])
         print('结束总结')
