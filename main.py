917d63e785a97d03d522ce31a63182cdd90097e7
E-sion <101786678+E-sion@users.noreply.github.com>
2023-09-12 22:51:03 +0800

    新增bot平台分支

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from waifu.Waifu import Waifu
-from waifu.StreamCallback import WaifuCallback
-from waifu.llm.GPT import GPT
-from waifu.llm.Claude import Claude
+import configparser
+
+from qqbot.qqbot import make_qq_bot
+from slackbot.slack_bot import make_slack_bot
 from tts.TTS import TTS
 from tts.edge.edge import speak
-from qqbot.qqbot import make_qq_bot
-from waifu.Tools import load_prompt, load_emoticon, load_memory, str2bool
-import configparser
+from waifu.StreamCallback import WaifuCallback
+from waifu.Tools import load_prompt, load_emoticon, load_memory, str2bool,load_im
+from waifu.Waifu import Waifu
+from waifu.llm.Claude import Claude
+from waifu.llm.GPT import GPT
 
 config = configparser.ConfigParser()
 
@@ -16,33 +18,34 @@ if len(config_files) == 0:
     raise FileNotFoundError('配置文件 config.ini 未找到，请检查是否配置正确！')
 
 # CyberWaifu 配置
-name 		 = config['CyberWaifu']['name']
-username     = config['CyberWaifu']['username']
-charactor 	 = config['CyberWaifu']['charactor']
-send_text    = str2bool(config['CyberWaifu']['send_text'])
-send_voice   = str2bool(config['CyberWaifu']['send_voice'])
-use_emoji 	 = str2bool(config['Thoughts']['use_emoji'])
-use_qqface   = str2bool(config['Thoughts']['use_qqface'])
+name = config['CyberWaifu']['name']
+username = config['CyberWaifu']['username']
+charactor = config['CyberWaifu']['charactor']
+send_text = str2bool(config['CyberWaifu']['send_text'])
+send_voice = str2bool(config['CyberWaifu']['send_voice'])
+use_emoji = str2bool(config['Thoughts']['use_emoji'])
+use_qqface = str2bool(config['Thoughts']['use_qqface'])
 use_emoticon = str2bool(config['Thoughts']['use_emoticon'])
-use_search 	 = str2bool(config['Thoughts']['use_search'])
-use_emotion  = str2bool(config['Thoughts']['use_emotion'])
-search_api	 = config['Thoughts_GoogleSerperAPI']['api']
-voice 		 = config['TTS']['voice']
+use_search = str2bool(config['Thoughts']['use_search'])
+use_emotion = str2bool(config['Thoughts']['use_emotion'])
+search_api = config['Thoughts_GoogleSerperAPI']['api']
+voice = config['TTS']['voice']
+slack_app_token = config["Slack"]["SLACK_APP_TOKEN"]
+slack_bot_token = config["Slack"]["SLACK_BOT_TOKEN"]
 
 prompt = load_prompt(charactor)
 
 # 语音配置
 tts_model = config['TTS']['model']
 if tts_model == 'Edge':
-	tts = TTS(speak, voice)
-	api = config['TTS_Edge']['azure_speech_key']
-	if api == '':
-		use_emotion = False
+    tts = TTS(speak, voice)
+    api = config['TTS_Edge']['azure_speech_key']
+    if api == '':
+        use_emotion = False
 
 # Thoughts 思考链配置
 emoticons = config.items('Thoughts_Emoticon')
 load_emoticon(emoticons)
-
 # LLM 模型配置
 model = config['LLM']['model']
 if model == 'OpenAI':
@@ -50,29 +53,35 @@ if model == 'OpenAI':
     callback = WaifuCallback(tts, send_text, send_voice)
     brain = GPT(openai_api, name, stream=True, callback=callback)
 elif model == 'Claude':
-	callback = None
-	user_oauth_token = config['LLM_Claude']['user_oauth_token']
-	bot_id = config['LLM_Claude']['bot_id']
-	brain = Claude(bot_id, user_oauth_token, name)
+    # claude为None
+    callback = None
+    user_oauth_token = config['LLM_Claude']['user_oauth_token']
+    bot_id = config['LLM_Claude']['bot_id']
+    brain = Claude(bot_id, user_oauth_token, name)
+# 启动平台配置
+im = config["IM"]["use_im"]
+load_im(im)
 
 waifu = Waifu(brain=brain,
-				prompt=prompt,
-				name=name,
-                username=username,
-				use_search=use_search,
-				search_api=search_api,
-				use_emoji=use_emoji,
-				use_qqface=use_qqface,
-                use_emotion=use_emotion,
-				use_emoticon=use_emoticon)
+              prompt=prompt,
+              name=name,
+              username=username,
+              use_search=use_search,
+              search_api=search_api,
+              use_emoji=use_emoji,
+              use_qqface=use_qqface,
+              use_emotion=use_emotion,
+              use_emoticon=use_emoticon)
 
 # 记忆导入
 filename = config['CyberWaifu']['memory']
 if filename != '':
-	memory = load_memory(filename, waifu.name)
-	waifu.import_memory_dataset(memory)
-
+    memory = load_memory(filename, waifu.name)
+    waifu.import_memory_dataset(memory)
 
 if model == 'OpenAI':
-	callback.register(waifu)
-make_qq_bot(callback, waifu, send_text, send_voice, tts)
+    callback.register(waifu)
+if im == "qq":
+    make_qq_bot(callback, waifu, send_text, send_voice, tts)
+elif im == "slack":
+    make_slack_bot(callback, waifu, send_text, send_voice, tts)
