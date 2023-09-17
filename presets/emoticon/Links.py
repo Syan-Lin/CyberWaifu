23cdf142ecb39aa27bf59061b50649521d4d15d6
E-sion <101786678+E-sion@users.noreply.github.com>
2023-09-17 14:21:26 +0800

    del

--- presets/emoticon/Links.py
+++
@@ -1,8 +0,0 @@
-linsk = {
-    '1.png': "https://s2.loli.net/2023/09/12/kcprXWNbOvw9iKh.png",
-    "2.png": "https://s2.loli.net/2023/09/12/g6ViXnTNEtBRGmz.png",
-    "3.png": "https://s2.loli.net/2023/09/12/dJqvKwX8PUEOmrh.png",
-    '4.png': "https://s2.loli.net/2023/09/12/tR27icWnhuzA8Ld.png",
-    "5.png": "https://s2.loli.net/2023/09/12/KPnHgXj2GhVRlJ9.png",
-    "6.png": "https://s2.loli.net/2023/09/12/ZEV2ysUlxWMNSO9.png"
-}
